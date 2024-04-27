75f18af7eb2f799b4dbd1d20bbec154df48f7c1f
daikibeppu <beppu.engineer@gmail.com>
2024-04-27 11:24:09 +0900

    📝 コーディング演習の追加

---
+++ JavaScript/coding_exercises/28_coding_exercises copy.js
@@ -0,0 +1,14 @@
+// * オブジェクトの演習問題
+// オブジェクトリテラルでオブジェクトを作りましょう！productという変数を作って、以下のプロパティを設定してくだださい：
+
+// name - Stringで'Icecream'
+// inStock - booleanでtrue
+// price - numberで198
+// flavors - 配列で、少なくとも3つのstringを定義してください（例えば['grape', 'apple', 'cherry']）
+
+const product = {
+  name: "Icecream",
+  inStock: true,
+  price: 198,
+  flavors: ["grape", "apple", "cherry"],
+};
