ee73d0a7be9b55b9da558cdbb095cd0c426aea40
daikibeppu <beppu.engineer@gmail.com>
2024-04-30 14:43:06 +0900

    📝 コーディング演習の追加

---
+++ JavaScript/coding_exercises/44_coding_exercises copy.js
@@ -0,0 +1,16 @@
+// * メソッドの演習問題
+// squareというオブジェクトを定義して、areaとperimeterというメソッドをもたせてください。
+// areaはsideという引数を一つ受け取って、sideを2乗した値を返します
+// perimeterはsideを受け取って、4を乗算した値を返します
+
+// square.area(10) //100
+// square.perimeter(10) //40
+
+const square = {
+  area: function (side) {
+    return side * side;
+  },
+  perimeter: function (side) {
+    return side * 4;
+  },
+};
---
+++ JavaScript/coding_exercises/44_coding_exercises.js
@@ -0,0 +1,22 @@
+// メソッドのthisの演習問題
+// henというオブジェクトを定義してください。
+// 2つのプロパティと、1つのメソッドを定義してください：
+
+// nameは'Helen'にしてください
+// eggCountは0にしてください
+// layAnEggというメソッドを定義してください。
+// このメソッドは、自分のeggCountを1加算して、'EGG'という文字列をreturnしてください。（thisを使う必要があります）
+
+// hen.name // "Helen"
+// hen.eggCount // 0
+// hen.layAnEgg() // "EGG"
+// hen.layAnEgg() // "EGG"
+// hen.eggCount // 2
+
+const hen = {
+  name: "Helen",
+  eggCount: 0,
+  layAnEgg: function () {
+    return (this.eggCount += 1), "EGG";
+  },
+};
