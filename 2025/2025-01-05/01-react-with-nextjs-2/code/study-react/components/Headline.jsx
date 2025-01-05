c0f446ed716d617c40a6190e9378b68ea79c8ede
daiki-beppu <beppu.engineer@gmail.com>
2025-01-05 21:56:13 +0900

    add: new til for 2025/01/05

---
+++ 2025/2025-01-05/01-react-with-nextjs-2/code/study-react/components/Headline.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import Image from 'next/image';
+
+import styles from '@/styles/Home.module.css';
+
+export default function Headline(props) {
+  return (
+    <div className={styles.main}>
+      <h1>{props.title} Page</h1>
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+      <ol>
+        <li>
+          Get started by editing <code>{props.title}.js</code>.
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+    </div>
+  );
+}
