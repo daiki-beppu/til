1b5a68ebd5be6c12451be3cb38439eb04e44aa35
daiki-beppu <beppu.engineer@gmail.com>
2025-01-06 09:25:31 +0900

    add: new til for 2025/01/06

--- 2025/2025-01-06/01-react-with-nextjs-3/code/study-react/src/components/Main/index.jsx
+++ 2025/2025-01-06/01-react-with-nextjs-3/code/study-react/src/components/Main/index.jsx
@@ -4,9 +4,17 @@ import Headline from 'src/components/Headline';
 import Links from 'src/components/Links';
 import styles from 'src/components/Main/Main.module.css';
 
+const handleClick = (e) => {
+  e.preventDefault();
+  alert('イベント発火！');
+};
+
 export default function Main(props) {
   return (
     <main className={styles.main}>
+      <a href="/about" onClick={handleClick}>
+        イベント処理
+      </a>
       <Headline title={props.title} />
       <Links />
     </main>
