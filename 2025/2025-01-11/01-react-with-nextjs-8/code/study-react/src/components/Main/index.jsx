1bfd3db6449ceddc1adb74105a849e9b9a9a124f
daiki-beppu <beppu.engineer@gmail.com>
2025-01-11 08:53:33 +0900

    add: new til for 2025/01/11

---
+++ 2025/2025-01-11/01-react-with-nextjs-8/code/study-react/src/components/Main/index.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+
+import Headline from 'src/components/Headline';
+import Links from 'src/components/Links';
+import { useCounter } from 'src/components/Main/hooks/useCounter';
+import { useInput } from 'src/components/Main/hooks/useInput';
+import { useSetBgColor } from 'src/components/Main/hooks/useSetBgColor';
+import styles from 'src/components/Main/Main.module.css';
+
+export default function Main(props) {
+  const { count, isView, handleClick, handleView } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInput();
+  useSetBgColor();
+
+  return (
+    <main className={styles.main}>
+      {isView ? <h1>{count}</h1> : null}
+      <button onClick={handleView}>{isView ? '非表示' : '表示'}</button>
+      <button onClick={handleClick}>イベント処理</button>
+
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+      <Headline title={props.title} />
+      <Links />
+    </main>
+  );
+}
